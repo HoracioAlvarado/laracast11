183824a378c81452b93ba6c577abbbc5e12ed2ab
Horacio Alvarado <halvarado@modernizacion.gob.ar>
2024-05-25 02:37:05 -0300

    Employer, Post, Tag

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,38 +1,21 @@
 <?php 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Job {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$40,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$8,000'
-            ]
-        ];
+class Job extends Model {
+    use HasFactory;
+    protected $table = 'job_listings';
+
+    protected $fillable = ['title', 'salary'];
+
+    public function employer() {
+        return $this->belongsTo(Employer::class);
     }
 
-    public static function findOrFail(int $id): ?array
+    public function tags()
     {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id );
-
-        if (! $job) {
-            abort(404);
-        }
-
-        return $job;
+        return $this->belongsToMany(Tag::class, foreignPivotKey: 'job_listing_id');
     }
 }
